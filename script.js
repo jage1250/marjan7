00a85ead56af308b03a922aeebbf305ee91f94b6
jage1250 <2415675radithya@kanisius.sch.id>
2026-05-23 21:13:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Selamat datang di Website Portofolio TIK - Radithya Javas Nararya (XI A4 / 17)");
-    
-    const buttons = document.querySelectorAll(".btn");
-    buttons.forEach(button => {
-        button.addEventListener("click", function() {
-            console.log("Navigasi internal dipicu menuju halaman detail project.");
-        });
-    });
 
-    const headerTitle = document.querySelector("header h1");
-    if(headerTitle) {
-        headerTitle.style.cursor = "pointer";
-        headerTitle.addEventListener("click", function() {
-            alert("Halo! Terima kasih sudah mengunjungi Portofolio TIK Radithya Javas Nararya Kelas XI A4.");
-        });
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Website Portofolio Radithya Javas Nararya XI A4 Berhasil Dimuat!");
+
+  
+    const title = document.querySelector("header h1");
+    title.addEventListener("click", function() {
+        alert("Halo Javas! Teruslah belajar pemrograman, error adalah bagian dari proses.");
+    });
 });
